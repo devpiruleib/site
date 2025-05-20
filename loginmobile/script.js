df1d4792eb4954a79d1871e709650ba0e2deda0f
piruleib <piruleibgames@gmail.com>
2025-05-20 19:23:33 -0300

    loginmobile update

--- loginmobile/script.js
+++ loginmobile/script.js
@@ -1,31 +1,26 @@
-const button = document.getElementById('dlmd');
-const imagem = document.getElementById('imagem');
+let botao = document.getElementById('changemode');
+let body = document.getElementById('body1');
+let container = document.getElementById('container');
+let imagem = document.getElementById('imagem');
+let nav = document.getElementById('nav1');
+let ativo = 0;
 
-function darkmode() {
-    document.getElementById('nav1').style.backgroundColor = "#1f1f2e"; // azul escuro
-    document.getElementById('nav1').style.color = "#f5f5f5";
-    document.getElementById('bode').style.backgroundColor = "#12121b"; // quase preto
-    document.getElementById('bode').style.color = "#f5f5f5";
-    document.getElementById('txtEmail').style.backgroundColor = "#2c2c3c";
-    document.getElementById('txtPassword').style.backgroundColor = "#2c2c3c";
-    document.getElementById('txtEmail2').style.backgroundColor = "#2c2c3c";
-    document.getElementById('txtPassword2').style.backgroundColor = "#2c2c3c";
-    document.getElementById('bode').style.transition = "0.5s";
+botao.addEventListener("click", () => {
+    ativo++;
+    container.style.transition = "0.5s";
+    nav.style.transition = "0.5s";
+    botao.style.transition = "0.5s";
+    
 
-    imagem.className = "fa-solid fa-sun fa-2xl";
-    imagem.style.color = "#ffd93b"; // amarelo vibrante
-
-    dlmd.addEventListener("click", function lightmode() {
-        document.getElementById('nav1').style.backgroundColor = "#e3f2fd"; // azul claro
-        document.getElementById('nav1').style.color = "#0d47a1"; // azul escuro
-        document.getElementById('bode').style.backgroundColor = "#ffffff";
-        document.getElementById('bode').style.color = "#0d47a1";
-        document.getElementById('txtEmail').style.backgroundColor = "#e1f5fe";
-        document.getElementById('txtPassword').style.backgroundColor = "#e1f5fe";
-        document.getElementById('txtEmail2').style.backgroundColor = "#e1f5fe";
-        document.getElementById('txtPassword2').style.backgroundColor = "#e1f5fe";
+    if(ativo === 1){
+        body1.className = "dark-mode";
+        imagem.className = "fa-solid fa-sun fa-2xl";
+        imagem.style.color = "#ffd93b"
+        
+    }else{
+        body1.className = "light-mode";
         imagem.className = "fa-solid fa-moon fa-2xl";
-        imagem.style.color = "#0d47a1"; // azul escuro
-        document.getElementById('bode').style.transition = "0.5s";
-    });
-}
+        imagem.style.color = "#0d47a1";
+        ativo = 0;
+    }
+})
